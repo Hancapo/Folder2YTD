0d25cd65e12ed1df3c2430fe92d9345c42337001
Vicente Jesus Lechuga <vicho0202@live.com>
2022-02-13 03:59:07 -0300

    Added support for DDS, JPG, PNG, BMP and TGA. Conversion seems to be fully working for now, I need to test more.

--- Folder2YTD/MainWindow.xaml.cs
+++ Folder2YTD/MainWindow.xaml.cs
@@ -17,6 +17,21 @@ using Ookii.Dialogs.Wpf;
 using System.Drawing;
 using System.IO;
 using Path = System.IO.Path;
+using BCnEncoder.Encoder;
+using BCnEncoder.Decoder;
+using BCnEncoder.Shared;
+using BCnEncoder.ImageSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using MessageBox = System.Windows.MessageBox;
+using MaterialDesignThemes.Wpf;
+using Color = System.Windows.Media.Color;
+using Image = SixLabors.ImageSharp.Image;
+using CodeWalker.Core;
+using CodeWalker.GameFiles;
+using CodeWalker.Utils;
+using System.Threading;
+using BCnEncoder.Shared.ImageFiles;
 
 namespace Folder2YTD
 {
@@ -25,117 +40,401 @@ namespace Folder2YTD
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<string> FoldersList = new();
+
         public MainWindow()
         {
             InitializeComponent();
-            TransparencyTypes.ItemsSource = new List<string>() {"Off", "By flags (PNG only)", "By pixels" };
-            FormatOutput.ItemsSource = new List<string>() {"GTA V (.YTD)" };
+            TransparencyTypes.ItemsSource = new List<string>() { "Off", "By pixels"};
+            FormatOutput.ItemsSource = new List<string>() { "GTA V (.YTD)" };
+            QualitySettings.ItemsSource = new List<string>() { "Fast", "Balanced", "Best Quality" };
             FormatOutput.SelectedIndex = 0;
-            TransparencyTypes.SelectedIndex = 0;    
+            TransparencyTypes.SelectedIndex = 1;
+            QualitySettings.SelectedIndex = 1;
+            GenerateMipMaps.IsChecked = true;
+
         }
 
         private void btnTesteo_Click(object sender, RoutedEventArgs e)
         {
 
-            
-            
+
         }
 
-        public bool IsTransparent(Bitmap image)
+        public bool IsTransparent(Image<Rgba32> image)
         {
             bool IsTransp = false;
 
-            if (TransparencyTypes.SelectedIndex == 1)
+            image.ProcessPixelRows(amigo =>
             {
-                //if (image.ToString().Contains(".tga"))
-                //{
+                for (int y = 0; y < amigo.Height; y++)
+                {
+                    Span<Rgba32> pixelRow = amigo.GetRowSpan(y);
 
-                //}
-                if ((image.Flags & 0x2) != 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            if (TransparencyTypes.SelectedIndex == 2)
-            {
-                for (int y = 0; y < image.Height; ++y)
-                {
-                    for (int x = 0; x < image.Width; ++x)
+                    foreach (ref Rgba32 pixel in pixelRow)
                     {
-                        if (image.GetPixel(x, y).A < 255)
+                        if (pixel.A < 255)
                         {
-                            return true;
+                            IsTransp = true;
+                            break;
                         }
                     }
                 }
-                return false;
-            }
-
-            if (TransparencyTypes.SelectedIndex == 0)
-            {
-                return false;
-            }
-
+            });
 
             return IsTransp;
+
+            
+
+        }
+
+        private void CreateYTDfilesFromFolders(List<string> Images, string forceoutput)
+        {
+
+
+            if (Images.Count != 0)
+            {
+
+                var ytd = new YtdFile();
+                ytd.TextureDict = new TextureDictionary();
+
+                string fname = Path.GetDirectoryName(forceoutput);
+
+                ytd.TextureDict.Textures = new ResourcePointerList64<Texture>();
+                ytd.TextureDict.TextureNameHashes = new ResourceSimpleList64_uint();
+                var data = ytd.Save();
+
+                ytd = TexturesToYTD(TextureListFromDDSFiles(Images.ToArray()), ytd);
+
+                data = ytd.Save();
+
+                var outfpath = forceoutput + ".ytd";
+                File.WriteAllBytes(outfpath, data);
+            }
+
+        }
+
+        public YtdFile TexturesToYTD(List<Texture> TexturesList, YtdFile ytdFile)
+        {
+            var textureDictionary = ytdFile.TextureDict;
+
+            textureDictionary.BuildFromTextureList(TexturesList);
+
+            return ytdFile;
+        }
+
+        public List<Texture> TextureListFromDDSFiles(string[] DdsFiles)
+        {
+            List<Texture> TextureList = new List<Texture>();
+
+            foreach (var DdsFile in DdsFiles)
+            {
+                var fn = DdsFile;
+
+                if (!File.Exists(fn)) return null; //couldn't find file?
+
+                try
+                {
+                    var dds = File.ReadAllBytes(fn);
+                    var tex = DDSIO.GetTexture(dds);
+                    tex.Name = Path.GetFileNameWithoutExtension(fn);
+                    tex.NameHash = JenkHash.GenHash(tex.Name?.ToLowerInvariant());
+                    JenkIndex.Ensure(tex.Name?.ToLowerInvariant());
+                    TextureList.Add(tex);
+
+                }
+                catch
+                {
+                    MessageBox.Show("Unable to load " + fn + ".\nAre you sure it's a valid .dds file?");
+                }
+            }
+
+            return TextureList;
 
         }
 
         private void btnSelectFolders_Click(object sender, RoutedEventArgs e)
         {
-           
-            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
-            dialog.Multiselect = true;
 
-            if ((bool)dialog.ShowDialog())
+            VistaFolderBrowserDialog vfbd = new()
             {
-                lbFolderView.ItemsSource = dialog.SelectedPaths;
+                Multiselect = true
+            };
+
+            if ((bool)vfbd.ShowDialog())
+            {
+                FoldersList = FoldersList.Concat(vfbd.SelectedPaths.ToList()).Distinct().ToList();
+
+                lbFolderView.ItemsSource = FoldersList;
+                LbProgressLog.Text = null;
+                lbFolderView.ScrollIntoView(lbFolderView.Items[0]);
+
             }
-            
+
         }
 
-        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        private async void btnConvert_Click(object sender, RoutedEventArgs e)
         {
+            LbProgressLog.Dispatcher.Invoke(() => { LbProgressLog.Text = String.Empty; });
 
+            if (FoldersList.Count == 0)
+            {
+                MessageBox.Show("Select some folders before converting.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                await YTDfromFolders(FoldersList).ConfigureAwait(false);
+            }
         }
 
         private async Task YTDfromFolders(List<string> AllFolders)
         {
-            string DXTFormat = string.Empty;
-            int MipMapCount = 0;
+            
 
             await Task.Run(() =>
             {
-                foreach (var folder in AllFolders)
+                for (int i = 0; i < AllFolders.Count; i++)
                 {
-                    var ImgFiles = Directory.EnumerateFiles(folder, "*.*", SearchOption.TopDirectoryOnly).Where(x => x.EndsWith(".png") || x.EndsWith(".dds"));
+
+                    List<string> ConvertedDDS = new();
+                    List<string> AlreadyDDSs = new();
+
+                    List<string> AllDDSmerged = new();
+
+                    string? folder = AllFolders[i];
+
+                    lbFolderView.Dispatcher.Invoke(() => { lbFolderView.SelectedIndex = i; });
+
+                    
+                    var ImgFiles = Directory.EnumerateFiles(folder, "*.*", SearchOption.TopDirectoryOnly).Where(x => x.EndsWith(".png") || x.EndsWith(".dds") || x.EndsWith(".jpg") || x.EndsWith(".tga") || x.EndsWith(".bmp"));
 
                     int ImgCount = ImgFiles.Count();
 
                     if (ImgCount <= 0)
                     {
-                        LbProgressLog.AppendText("\n0 compatible textures, skipping...");
+
+                        
+                        LbProgressLog.Dispatcher.Invoke(() => { LbProgressLog.AppendText("\n0 compatible textures, skipping..."); });
+
                     }
                     else
                     {
-                        LbProgressLog.AppendText("\n" + ImgCount + " compatible textures.");
+                        LbProgressLog.Dispatcher.Invoke(() => { LbProgressLog.AppendText("\n" + ImgCount + " compatible textures."); });
 
                     }
 
+
                     foreach (var imgfile in ImgFiles)
                     {
+
+
                         if (ImgFiles.Any())
                         {
                             string ImgFileName = Path.GetFileName(imgfile);
                             string ImgFileNameWithoutExt = Path.GetFileNameWithoutExtension(imgfile);
+
+                            if (ImgFileName.EndsWith(".png") || ImgFileName.EndsWith(".tga") || ImgFileName.EndsWith(".jpg") || ImgFileName.EndsWith(".bmp"))
+                            {
+
+                                LbProgressLog.Dispatcher.Invoke(() => { LbProgressLog.AppendText($"\nImage file {ImgFileName} found..."); }) ;
+
+                                string NewFolder = $"{Path.GetDirectoryName(imgfile)}/converted_dds/";
+
+                                if (!Directory.Exists(NewFolder))
+                                {
+                                    Directory.CreateDirectory(NewFolder);
+                                }
+
+                                LbProgressLog.Dispatcher.Invoke(() => { LbProgressLog.AppendText($"\nMoving {ImgFileName} to a new folder."); });
+
+                                string MovedImageName = $"{NewFolder}{ImgFileName}";
+
+                                File.Copy(imgfile, MovedImageName, true);
+
+                                ConvertImageToDDS(MovedImageName);
+
+                                LbProgressLog.Dispatcher.Invoke(() => { LbProgressLog.AppendText($"\nConverting {ImgFileName} to DDS..."); });
+
+
+                                File.Delete(MovedImageName);
+
+
+
+
+                            }
+                            if (ImgFileName.EndsWith(".dds"))
+                            {
+                                AlreadyDDSs.Add(imgfile);
+                            }
+
+
+                            
+
                         }
                     }
+
+
+                    ConvertedDDS = Directory.GetFiles(folder + "/converted_dds/", "*.dds", SearchOption.TopDirectoryOnly).ToList();
+
+
+                    AllDDSmerged.AddRange(ConvertedDDS);
+                    AllDDSmerged.AddRange(AlreadyDDSs);
+
+                    LbProgressLog.Dispatcher.Invoke(() => { LbProgressLog.AppendText($"\nCreating {Path.GetFileName(folder)}.ytd file..."); });
+
+
+                    CreateYTDfilesFromFolders(AllDDSmerged, folder);
+
+
+                    Directory.Delete(folder + "/converted_dds", true); 
+
+
                 }
+
+                MessageBox.Show($"Done, {AllFolders.Count} folder(s) processed.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
             });
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            FoldersList.Clear();
+            lbFolderView.ItemsSource = null;
+            LbProgressLog.Text = String.Empty;
+        }
+
+
+
+
+
+     
+
+
+
+
+        private void ConvertImageToDDS(string filename)
+        {
+
+            bool isChecked = false;
+
+            int QualitySelectedIndex = -1;
+
+            int TransparencyDetection = -1;
+
+
+            Image<Rgba32> image = Image.Load<Rgba32>(filename);
+            
+            BcEncoder bcEncoder = new BcEncoder();
+
+            bcEncoder.OutputOptions.FileFormat = OutputFileFormat.Dds;
+
+            
+
+            GenerateMipMaps.Dispatcher.Invoke(() => { isChecked = (bool)GenerateMipMaps.IsChecked; });
+
+            QualitySettings.Dispatcher.Invoke(() => { QualitySelectedIndex = QualitySettings.SelectedIndex; });
+
+            TransparencyTypes.Dispatcher.Invoke(() => { TransparencyDetection = TransparencyTypes.SelectedIndex; });
+
+            if (isChecked)
+            {
+                bcEncoder.OutputOptions.GenerateMipMaps = true;
+            }
+            else
+            {
+                bcEncoder.OutputOptions.GenerateMipMaps = false;
+            }
+
+            switch (QualitySelectedIndex)
+            {
+                case 0:
+                    bcEncoder.OutputOptions.Quality = CompressionQuality.Fast;
+                    break;
+                case 1:
+                    bcEncoder.OutputOptions.Quality = CompressionQuality.Balanced;
+                    break;
+
+                case 2:
+                    bcEncoder.OutputOptions.Quality = CompressionQuality.BestQuality;
+                    break;
+
+                default:
+                    bcEncoder.OutputOptions.Quality = CompressionQuality.Balanced;
+                    break;
+            }
+
+
+            if (TransparencyDetection == 1)
+            {
+                if (IsTransparent(image))
+                {
+                    bcEncoder.OutputOptions.Format = CompressionFormat.Bc3;
+                }
+                else
+                {
+                    bcEncoder.OutputOptions.Format = CompressionFormat.Bc1;
+
+                }
+            }
+            else
+            {
+                bcEncoder.OutputOptions.Format = CompressionFormat.Bc3;
+
+            }
+
+
+
+            string GetImageName = Path.GetFileNameWithoutExtension(filename);
+
+            //FileStream fs = File.Create(Path.GetDirectoryName(filename) + "/" + GetImageName + ".dds");
+
+
+
+            var amigo = bcEncoder.EncodeToDds(image);
+
+            MemoryStream ms = new MemoryStream();
+
+            amigo.Write(ms);
+
+            byte[] ddsbytes = ms.ToArray();
+            
+
+            File.WriteAllBytes(Path.GetDirectoryName(filename) + "/" + GetImageName + ".dds", ddsbytes);
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnHelpabout_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("This program was created to convert one or more folder with textures in its interior to a fully working .YTD\n\n● Quality settings are intended for its use in non-DDS files to convert them to .DDS with the selected quality.\n\n● Transparency detection works to determinate transparency for non-DDS files, in order to create a .DDS with proper compression, particularly useful when you are trying to bring textures to memory constrained enviroments such as FiveM servers.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void spToolbar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void lbFolderView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lbFolderView.ScrollIntoView(lbFolderView.SelectedItem);
+        }
+
+        private void LbProgressLog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            LbProgressLog.ScrollToEnd();
         }
     }
 }
